282ec521302de658efcf22b05e82bda03ab777c9
Gustavo Tona <gustavopicolitona@gmail.com>
2024-01-20 13:37:01 -0300

    setup update

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-60=f3fjm-o(sg3garo2w*vb0wald%8h22l&%ojqgk8p8y1a^z-'
+SECRET_KEY = config("SECRET_KEY") #colocamos na pynthon.couple .env 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

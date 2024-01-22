04a0e56ec11bf7acbbcdd90539f6736e40ebe17f
Gustavo Tona <gustavopicolitona@gmail.com>
2024-01-21 23:18:32 -0300

    Update views.py

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse # deve puxar o http response
-# Create your views here.
+# deve puxar o http response
 
-def home(request):
-    return render(request, "todos/home.html") #colocar o render para usar o html 
+from .models import Todo  # vamos na pasta models pegar a classe Todo
+
+def todo_list(request):
+    todos = Todo.objects.all() #classe de modelo precisa de um objects para pegas no banco de dados
+
+#colocar o render para renderizar o html 
+    return render(request, "todos/todo_list.html", {"todos": todos}) #nome pegar do banco de dados e aparecer no html
 
 #deve ser criada uma pasta com o nome template depois o nome do projeto e o html
